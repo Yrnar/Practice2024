8a631d2645294d7f886f6853b1ee5ecef5c939c3
Yrnar <ernar030913@gmail.com>
2024-06-10 15:06:15 +0500

    feedback update

--- fb/customer_feedback/feedback/models.py
+++ fb/customer_feedback/feedback/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
 class Feedback(models.Model):
+    RATING_CHOICES = [
+        ('very bad', 'Very Bad'),
+        ('very good', 'Very Good'),
+    ]
+
     customer_name = models.CharField(max_length=100)
-    rating = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
+    rating = models.CharField(max_length=10, choices=RATING_CHOICES)
     comment = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
--- fb/customer_feedback/feedback/serializers.py
+++ fb/customer_feedback/feedback/serializers.py
@@ -5,8 +5,3 @@ class FeedbackSerializer(serializers.ModelSerializer):
     class Meta:
         model = Feedback
         fields = ['customer_name', 'rating', 'comment', 'created_at']
-
-    def validate_rating(self, value):
-        if value < 1 or value > 5:
-            raise serializers.ValidationError("Rating must be between 1 and 5.")
-        return value
